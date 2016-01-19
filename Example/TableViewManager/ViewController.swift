00797002afc9ed26b9942349166eee931510b356
Artur Jaworski <kontakt@arturjaworski.pl>
2016-01-19 13:19:23 +0100

    Initial commit

---
+++ Example/TableViewManager/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TableViewManager
+//
+//  Created by Artur Jaworski on 01/19/2016.
+//  Copyright (c) 2016 Artur Jaworski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
